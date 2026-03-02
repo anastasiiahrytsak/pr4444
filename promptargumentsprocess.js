1cb039ac5e91594a8164c4cc9df25be8a038c36f
anastasiiahrytsak <anastasiia.m.hrytsak@ukd.edu.ua>
2026-03-02 10:31:26 +0200

    Add files via upload

---
+++ promptargumentsprocess.js
@@ -0,0 +1,19 @@
+const args = process.argv.slice(2); 
+
+if (args.length < 2) {
+  console.error(" Помилка: Введіть ім'я та вік! (Приклад: node script.js Олексій 20)");
+  process.exit(1);
+}
+
+const [name, age] = args;
+
+console.log(`
+   Привіт, ${name}!`);
+
+if (Number(age) >= 18) {
+  console.log(" Доступ дозволено: Ви вже дорослий програміст.");
+} else {
+  console.log(" Доступ обмежено: Тобі ще зарано дебажити ядро.");
+}
+
+
