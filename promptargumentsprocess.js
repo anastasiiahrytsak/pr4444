64f279729665b3bb22351405aa2cd59b7fb9bc2b
anastasiiahrytsak <anastasiia.m.hrytsak@ukd.edu.ua>
2026-03-02 10:40:08 +0200

    Update promptargumentsprocess.js

--- promptargumentsprocess.js
+++ promptargumentsprocess.js
@@ -1,19 +1,18 @@
 const args = process.argv.slice(2); 
 
+
 if (args.length < 2) {
-  console.error(" Помилка: Введіть ім'я та вік! (Приклад: node script.js Олексій 20)");
+  console.error(" Помилка: Введіть ім'я та вік! (Приклад: node script.js Анастасія 17)");
   process.exit(1);
 }
 
 const [name, age] = args;
 
-console.log(`
-   Привіт, ${name}!`);
+console.log(`Привіт, ${name}!`);
 
+// Перетворюємо рядок на число для порівняння
 if (Number(age) >= 18) {
   console.log(" Доступ дозволено: Ви вже дорослий програміст.");
 } else {
-  console.log(" Доступ обмежено: Тобі ще зарано дебажити ядро.");
+  console.log(" Доступ обмежено");
 }
-
-
